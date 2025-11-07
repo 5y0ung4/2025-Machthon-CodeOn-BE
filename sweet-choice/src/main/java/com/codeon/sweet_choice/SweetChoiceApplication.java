131ebd56f8d6403dad2f95405e3f00d43f8152a1
5y0ung4 <qwerrr504@naver.com>
2025-11-07 21:19:30 +0900

    개발 폴더 세팅

---
+++ sweet-choice/src/main/java/com/codeon/sweet_choice/SweetChoiceApplication.java
@@ -0,0 +1,13 @@
+package com.codeon.sweet_choice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetChoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetChoiceApplication.class, args);
+	}
+
+}
