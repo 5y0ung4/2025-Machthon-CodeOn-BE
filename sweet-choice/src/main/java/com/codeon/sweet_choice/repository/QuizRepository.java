7cb9208db360560bbb8fce2fd6ab67ee92d0c748
soy1040 <soy10401@gmail.com>
2025-11-14 12:57:00 +0900

    Revert "feature(quiz): 퀴즈불러오기, 퀴즈 정답 풀이"
    This reverts commit d8ee39800a7b3095f3b7c9ff6d50ebf812011e70.

--- sweet-choice/src/main/java/com/codeon/sweet_choice/repository/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codeon.sweet_choice.repository;
-
-import com.codeon.sweet_choice.entity.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-    Quiz findByQuizId(Long quizId);
-}
